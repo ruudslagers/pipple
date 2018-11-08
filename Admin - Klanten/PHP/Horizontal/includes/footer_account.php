f6d62f928d3ce75f3fa4ce4077e00d97265f1d9c
Unknown <r.slagers@student.fontys.nl>
2018-11-08 10:06:57 +0100

    Admin - Klanten aangemaakt

---
+++ Admin - Klanten/PHP/Horizontal/includes/footer_account.php
@@ -0,0 +1,27 @@
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.nicescroll.js"></script>
+        <script src="assets/plugins/switchery/switchery.min.js"></script>
+        
+        <!-- Counter Up  -->
+        <script src="assets/plugins/waypoints/lib/jquery.waypoints.min.js"></script>
+        <script src="assets/plugins/counterup/jquery.counterup.min.js"></script>
+
+        <!-- KNOB JS -->
+        <!--[if IE]>
+        <script type="text/javascript" src="assets/plugins/jquery-knob/excanvas.js"></script>
+        <![endif]-->
+        <script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
+
+        <script src="assets/plugins/multiselect/js/jquery.multi-select.js"></script>
+        <!-- Peity chart js -->
+        <script src="assets/plugins/peity/jquery.peity.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+    </body>
+</html>
